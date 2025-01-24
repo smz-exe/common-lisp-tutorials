295d91fa9f250fd59e517a4dc79dc6f30116f017
smz-exe <shimizu-yuki13861@keio.jp>
2025-01-24 11:01:27 +0900

    Add additional examples and improve formatting in Lisp files

--- examples/remove-if.lisp
+++ examples/remove-if.lisp
@@ -1,5 +1,5 @@
 (defun numbrep (x)
-    (numberp x))
+  (numberp x))
 (remove-if #'numbrep '(1 2 3 a b c 4 5 6))
 
 (remove-if #'(lambda (x) (not (plusp x))) '(2 0 4 -4 -8 0 -9))
@@ -7,18 +7,18 @@
 (remove-if-not #'(lambda (x) (plusp x)) '(2 0 4 -4 -8 0 -9))
 
 (defun pick (x)
-    (remove-if-not #'(lambda (e) (and (> e 1) (< e 5))) x))
+  (remove-if-not #'(lambda (e) (and (> e 1) (< e 5))) x))
 
 (pick '(1 2 3 4 5 6 7 8 9 0))
 
 (defun count-the (x)
-    (length (remove-if-not #'(lambda (e) (equal e 'the)) x)))
+  (length (remove-if-not #'(lambda (e) (equal e 'the)) x)))
 
 (count-the '(the the a the a the a the))
 
 (defun my-intersection (x y)
-    (remove-if-not #'(lambda (e) (member e y)) x))
-(my-intersection '(a b ) '(a b c d))
+  (remove-if-not #'(lambda (e) (member e y)) x))
+(my-intersection '(a b) '(a b c d))
 
 (find-if #'oddp '(2 4 6 7 8))
 
--- examples/test.lisp
+++ examples/test.lisp
@@ -5,44 +5,44 @@
 (rest (list (first '(1 2))))
 (listp '(1 2))
 (first '(1 2))
-(first (list(rest '(1 2))))
-(defun fact(n)
-    (if (= n 0) 1 (* n (fact (- n 1)))))
+(first (list (rest '(1 2))))
+(defun fact (n)
+  (if (= n 0) 1 (* n (fact (- n 1)))))
 (fact 5)
 (fact 10)
 
 (defun len (x)
-    (if (null x) 0 (+ 1 (len (rest x)))))
+  (if (null x) 0 (+ 1 (len (rest x)))))
 (len '(1 2 3 4 5))
 ;;; (my-append '(a b) '(c d))
 ;;; a (my-append '(b) '(c d))
 ;;; b (my-append '() '(c d))
 
 (defun my-append (x y)
-    (if (null x) y (cons (first x) (my-append (rest x) y))))
+  (if (null x) y (cons (first x) (my-append (rest x) y))))
 (my-append '(a b) '(c d))
 (my-append '(1 5 3) '(4 2 6))
 
 (defun add-up (x)
-    (if (null x) 0 (+ (first x) (add-up (rest x)))))
+  (if (null x) 0 (+ (first x) (add-up (rest x)))))
 (add-up '(11 12 13 14 15))
 
 (defun laugh (x)
-    (if (zerop x) nil (cons 'HA (laugh (- x 1)))))
+  (if (zerop x) nil (cons 'HA (laugh (- x 1)))))
 (laugh 6)
 
 (defun count-down (x)
-    (if (zerop x) nil (cons x (count-down (- x 1)))))
+  (if (zerop x) nil (cons x (count-down (- x 1)))))
 (count-down 5)
 
 (defun count-down2 (x)
-    (if (equal x 1) '(1) (cons x (count-down2 (- x 1)))))
+  (if (equal x 1) '(1) (cons x (count-down2 (- x 1)))))
 (count-down 10)
 
 (defun count-odd (x)
-    (if (null x) nil (if (= (mod (first x) 2) 0) (count-odd (rest x)) (cons (first x) (count-odd (rest x))))))
+  (if (null x) nil (if (= (mod (first x) 2) 0) (count-odd (rest x)) (cons (first x) (count-odd (rest x))))))
 
-(count-odd '(10 5  2 4 6 3 7 8 9))
+(count-odd '(10 5 2 4 6 3 7 8 9))
 
 (append '(a b) '(c d))
 
@@ -66,32 +66,32 @@
 
 ; 1
 (defun rev (x)
-    (if (null x) nil (append (rev (rest x)) (list (first x)))))
+  (if (null x) nil (append (rev (rest x)) (list (first x)))))
 
 (rev '(1 2 3 4))
 
 (defun rm (x y)
-    (cond ((null y) nil)
-          ((equal x (first y)) (rm ()))))
+  (cond ((null y) nil)
+        ((equal x (first y)) (rm ()))))
 
 (defun mem (x y)
-    (if (null y) nill (if (equal (x (first y))) y ())))
+  (if (null y) nill (if (equal (x (first y))) y ())))
 
 
 ; 4
 (defun mem (x y)
-    (cond ((null y) nil)
-          ((equal x (first y)) y)
-          (t (mem x (rest y)))))
+  (cond ((null y) nil)
+        ((equal x (first y)) y)
+        (t (mem x (rest y)))))
 
 ; 5
 (defun berforeep (x y l)
-    (mem y (mem x l)))
+  (mem y (mem x l)))
 
 
 ; 7
 
-(defun rev(x)
-    (if (null x) nil (append rev(rest x) (first x))))
+(defun rev (x)
+  (if (null x) nil (append rev (rest x) (first x))))
 
 (rest '(1 2 3 4))
